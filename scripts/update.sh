6e4ba51988f81dde3d4b2eb1a493fbd9aa22c70b
nwags <nickwisc@gmail.com>
2015-11-12 09:37:56 -0500

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,11 +6,11 @@ cd /home/pi/
 pip install pyyaml
 sudo apt-get install -y python-yaml
 
-sudo rm -rf /home/pi/otone_backend_rsa
-sudo cp -al /home/pi/otone_backend /home/pi/otone_backend_rsa
+sudo rm -rf /home/pi/otone_backend_bak
+sudo cp -al /home/pi/otone_backend /home/pi/otone_backend_bak
 
-sudo rm -rf /home/pi/otone_frontend_rsa
-sudo cp -al /home/pi/otone_frontend /home/pi/otone_frontend_rsa
+sudo rm -rf /home/pi/otone_frontend_bak
+sudo cp -al /home/pi/otone_frontend /home/pi/otone_frontend_bak
 
 sudo rm -rf /home/pi/otone_backend
 sudo rm -rf /home/pi/otone_frontend
@@ -34,6 +34,14 @@ echo '/home/pi/otone_scripts/gt_backend:'
 echo $(cat /home/pi/otone_scripts/gt_backend)
 echo ''
 
+if [[ "$B_FF" == "Cloning into" && "$B_AR" == "Receiving objects: 100% Receiving objects: 100%" && "$B_CC" == "Checking connectivity... done."]]; then
+	echo '!ot!!update!success!msg:way to go!'
+else
+	echo '!ot!!update!failure!msg:update failed, you may try again...'
+	sudo cp -al /home/pi/otone_backend_bak /home/pi/otone_backend
+fi
+
+
 git clone --progress git@github.com:Opentrons/otone_frontend.git > /home/pi/otone_scripts/gt_frontend 2>&1
 
 F_FF=$(grep -o 'Cloning into' /home/pi/otone_scripts/gt_frontend)
@@ -47,5 +55,12 @@ echo ''
 echo '/home/pi/otone_scripts/gt_frontend:'
 echo $(cat /home/pi/otone_scripts/gt_frontend)
 echo ''
+
+if [[ "$F_FF" == "Cloning into" && "$F_AR" == "Receiving objects: 100% Receiving objects: 100%" && "$F_CC" == "Checking connectivity... done."]]; then
+	echo '!ot!!update!success!msg:way to go!'
+else
+	echo '!ot!!update!failure!msg:update failed, you may try again...'
+	sudo cp -al /home/pi/otone_frontend_bak /home/pi/otone_frontend
+fi
 sudo systemctl disable otone.service
 sudo systemctl stop otone.service
