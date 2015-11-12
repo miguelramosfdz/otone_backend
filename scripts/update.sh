780a51dc442f2f2593398003fe0cf896ffbf2e3d
nwags <nickwisc@gmail.com>
2015-11-12 09:22:23 -0500

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,9 @@
 
 cd /home/pi/
 
+pip install pyyaml
+sudo apt-get install -y python-yaml
+
 sudo rm -rf /home/pi/otone_backend_rsa
 sudo cp -al /home/pi/otone_backend /home/pi/otone_backend_rsa
 
@@ -22,20 +25,27 @@ git clone --progress git@github.com:Opentrons/otone_backend.git > /home/pi/otone
 B_FF=$(grep -o 'Cloning into' /home/pi/otone_scripts/gt_backend)
 B_AR=$(grep -o 'Receiving objects: 100%' /home/pi/otone_scripts/gt_backend)
 B_CC=$(grep -o 'Checking connectivity... done.' /home/pi/otone_scripts/gt_backend)
+echo ''
 echo 'B_FF = '$B_FF
 echo 'B_AR = '$B_AR
 echo 'B_CC = '$B_CC
+echo ''
+echo '/home/pi/otone_scripts/gt_backend:'
 echo $(cat /home/pi/otone_scripts/gt_backend)
+echo ''
 
 git clone --progress git@github.com:Opentrons/otone_frontend.git > /home/pi/otone_scripts/gt_frontend 2>&1
 
 F_FF=$(grep -o 'Cloning into' /home/pi/otone_scripts/gt_frontend)
 F_AR=$(grep -o 'Receiving objects: 100%' /home/pi/otone_scripts/gt_frontend)
 F_CC=$(grep -o 'Checking connectivity... done.' /home/pi/otone_scripts/gt_frontend)
+echo ''
 echo 'F_FF = '$F_FF
 echo 'F_AR = '$F_AR
 echo 'F_CC = '$F_CC
+echo ''
+echo '/home/pi/otone_scripts/gt_frontend:'
 echo $(cat /home/pi/otone_scripts/gt_frontend)
-
+echo ''
 sudo systemctl disable otone.service
 sudo systemctl stop otone.service
